c29c42b9a627d4d09ffdded30fa63a923eb74b24
oaama <angluosr@gmail.com>
2025-05-16 01:14:47 +0300

    Refactor course and user routes: streamline course creation, enforce authentication for user actions, and remove unused course update/delete routes.

--- models/Video.js
+++ models/Video.js
@@ -4,7 +4,7 @@ const videoSchema = new mongoose.Schema({
   title: { type: String, required: true }, // عنوان الفيديو
   videoPath: { type: String, required: true }, // مسار الفيديو
   course: { type: mongoose.Schema.Types.ObjectId, ref: 'Course', required: true }, // الكورس المرتبط
-  order: { type: Number, required: true }, // ترتيب الفيديو داخل الكورس
+  order: { type: Number, required: false }, // ترتيب الفيديو داخل الكورس
 });
 
 module.exports = mongoose.model('Video', videoSchema);
